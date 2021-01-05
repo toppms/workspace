146ce61fcdb6408d10f49c31d953b9efbec094a0
toppms <top1604@naver.com>
2021-01-05 18:38:13 +0900

    practice2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const title = document.querySelector("#title");
 title.innerHTML = "Hi From JS";
-title.style.color = "red";
 console.log(title);
 document.title = "I own you now";
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+function init() {
+    title.addEventListener("click",handleClick);
+}
+init();
